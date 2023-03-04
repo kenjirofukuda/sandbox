2fc84cbeda2aded5a81c92db184f4644d5f866b0
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2023-03-04 17:13:47 +0900

    initial

---
+++ ImageFun/AppController.h
@@ -0,0 +1,39 @@
+/* 
+   Project: ImageFun
+
+   Author: Kenjiro Fukuda,,,
+
+   Created: 2023-03-04 17:04:01 +0900 by kenjiro
+   
+   Application Controller
+*/
+ 
+#ifndef _PCAPPPROJ_APPCONTROLLER_H
+#define _PCAPPPROJ_APPCONTROLLER_H
+
+#import <AppKit/AppKit.h>
+// Uncomment if your application is Renaissance-based
+//#import <Renaissance/Renaissance.h>
+
+@interface AppController : NSObject
+{
+}
+
++ (void)  initialize;
+
+- (id) init;
+- (void) dealloc;
+
+- (void) awakeFromNib;
+
+- (void) applicationDidFinishLaunching: (NSNotification *)aNotif;
+- (BOOL) applicationShouldTerminate: (id)sender;
+- (void) applicationWillTerminate: (NSNotification *)aNotif;
+- (BOOL) application: (NSApplication *)application
+	    openFile: (NSString *)fileName;
+
+- (void) showPrefPanel: (id)sender;
+
+@end
+
+#endif
---
+++ ImageFun/ImageFun_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: ImageFun
+
+   Author: Kenjiro Fukuda,,,
+
+   Created: 2023-03-04 17:04:01 +0900 by kenjiro
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
