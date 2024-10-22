18e522ce34d8194522d690132740d20ac52f6bc4
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2024-10-22 09:22:04 +0900

    use distclean

--- Tools/all_backend.sh
+++ Tools/all_backend.sh
@@ -5,14 +5,14 @@
 ./configure --enable-graphics=xlib --with-name=xlib 
 make
 sudo -E make install
-make clean
+make distclean
 
 ./configure --enable-graphics=art --with-name=art 
 make
 sudo -E make install
-make clean
+make distclean
 
 ./configure --enable-graphics=cairo --with-name=cairo 
 make
 sudo -E make install
-make clean
+make distclean
