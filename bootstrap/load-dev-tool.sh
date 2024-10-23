6ecb2a1592164cdca8eb818f161006e0c2cb5e91
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2024-10-23 13:43:26 +0900

    more check

--- bootstrap/load-dev-tool.sh
+++ bootstrap/load-dev-tool.sh
@@ -1,9 +1,60 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 echo "Hello dev tool"
 echo $(uname)
 INSTALL_CMD="sudo apt install -y"
+case $(uname) in
+  *Linux)
+    ;;
+  *BSD)
+    INSTALL_CMD="sudo pkg install -y"
+    ;;
+  *)
+    echo "!!! Sorry Unsupported. !!! "
+    exit 1
+    ;;
+esac
 
+function check_cmd {
+  local cmd=$1
+  local pkg=$2
+  if [ -z "$pkg" ]; then
+    pkg=$cmd
+  fi
+  echo "=========================="
+  echo "    check for $cmd ...    "
+  local reply=$(which $cmd)
+  # echo "reply -> [$reply]"
+  if [ -z "$reply" ]; then
+    echo "$cmd not found"
+    echo "$INSTALL_CMD $pkg"
+  else
+    echo "$cmd --> [${reply}]"
+  fi
+  echo "=========================="
+}
+
+function install_vimrc {
+if [ ! -f ~/.vimrc ]; then
+cat <<EOF_VIMRC >> ~/.vimrc
+set number
+syntax on
+set expandtab
+set shiftwidth=2
+set softtabstop=2
+set tabstop=2
+EOF_VIMRC
+fi
+}
+
+check_cmd git
+check_cmd gh
+check_cmd clang
+check_cmd vim
+check_cmd emacs
+check_cmd curl
+check_cmd locate2 plocate
+install_vimrc
 
 cat <<EOF > /dev/null
 Usage: uname [OPTION]...
