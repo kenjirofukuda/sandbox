028a429422287c7c0feb539920e62bfcb508e96e
Kenjiro Fukuda <kenjirofukuda@gmail.com>
2024-10-25 16:32:59 +0900

    remove gnusstep app projects

--- Tools/clang-cmakeinit.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE string "clang compiler" FORCE)
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE string "clang++ compiler" FORCE)
-set(CMAKE_LINKER "ld.gold")
-set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=/usr/bin/ld.gold")
--- Tools/u2204-uselibs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-grep -v -e '^#' u2204-prerequires.txt \
-    | grep -v -e '^$' \
-    | sed 's/^/sudo apt-get install /g' \
-    | sh
